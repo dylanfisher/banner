672bb0779c12d2bbe7da1d4a825d38f1f0ee110c
dylanfisher <hi@dylanfisher.com>
2014-04-13 12:21:00 -0400

    add support for home page infobox text

--- page-home.php
+++ page-home.php
@@ -5,10 +5,8 @@ Template Name: Page - Home
 get_header(); ?>
     <div class="content"><?php the_post() ?>
         <div id="info-box" class="info-box draggable">
-            <p><span class="date">Spring 2014</span></p>
-            <p>Website coming soon!</p>
-            <p>Visit us at ICFF Booth #2524 at New York's Javits Center, May 17-20, 2014 </p>
-            <p>Sign up here to be the first to hear about the new collection:</p>
+            <p><span class="date"><?php the_field('infobox_date'); ?></span></p>
+            <?php the_field('infobox_text'); ?>
             <div class="email-form-container">
                 <input id="email-form" type="email" value="" type="text" placeholder="Your email here">
                 <p id="email-button" class="email-button">Submit</p>
