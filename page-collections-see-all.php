d5a2e03b791b5f565833d393c5018134c6e27ec9
dylanfisher <hi@dylanfisher.com>
2014-04-17 22:26:25 -0400

    undo heading inside link

--- page-collections-see-all.php
+++ page-collections-see-all.php
@@ -26,7 +26,9 @@ $the_query = new WP_Query( $args ); ?>
                     <a class="api-product nu" data-slug="<?php echo $post->post_name; ?>" href="<?php the_permalink(); ?>">
                 <?php echo wp_get_attachment_image( $attachment_id, $size ); ?>
                     </a>
-                    <h2><a class="api-product nu title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <a class="api-product nu title" href="<?php the_permalink(); ?>">
+                        <h2><?php the_title(); ?></h2>
+                    </a>
                 </div>
     <?php endwhile; // End loop ?>
     <?php wp_reset_postdata(); ?>
